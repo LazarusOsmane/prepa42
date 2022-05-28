999684b3c4d45008e03b0564e4fba77d00986195
LazarusOsmane <88276778+LazarusOsmane@users.noreply.github.com>
2022-05-28 15:49:36 +0200

    Update main.c
    Correction de main.

--- main.c
+++ main.c
@@ -4,3 +4,17 @@ int main()
 {
 	printf("hello world\n");
 }
+
+/*
+CORRECTOIN: 
+
+#include <stdio.h>
+
+int main(void)                      // Par convention quand une fonction na pas de paramettre, on ecrit void.
+{
+	printf("hello world\n");
+	return (0);                // A la fin d'un main ne jammais oublier return (0); cela permet d'inquer que ton programme c'est terminer correctement.
+}
+*/
+
+
